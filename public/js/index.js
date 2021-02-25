a713d276911d746457c9b75de773110767841dd8
Vikas Verma <vikasverma@Vikass-MacBook-Pro.local>
2021-02-25 16:37:21 -0500

    Added browserify

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,14 @@
 
-$document.ready(function(){
-    $('[data-toggle="popover"]').popover();   
-    });
+// const jsdom = require("jsdom")
+// const { JSDOM } = jsdom
+// const { window } = new JSDOM(`<!DOCTYPE html>`)
+// const $ = require('jQuery')(window)
+
+const uniqid = require('uniqid')
+
+document.querySelector("#juhi").innerHTML = uniqid()
+
+// $document.ready(function(){
+//     document.querySelector("#juhi").innerHTML = uniqid()
+//     $('[data-toggle="popover"]').popover();   
+//     });
